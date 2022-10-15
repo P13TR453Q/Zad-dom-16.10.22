5bc2044f4ad977c3e076a47bdfa9ddc9f8592e6d
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-10-15 19:41:04 +0000

    Zrobiono zad 1-4 z Karty Pracy

---
+++ KartaPracy2a.py
@@ -0,0 +1,28 @@
+#Zad 1
+# a = int(input())
+# b = int(input())
+# if (a + b) / 2:  print("yes")
+# else:  print("no")
+#Zad 2
+# a = int(input())
+# g = int(input())
+# if ((a + g)/ 2) > ((a * g) //3): 
+#   print("yes")
+# else:  print("no")
+#Zad 3
+# k = int(input())
+# l = int(input())
+# m = int(input())
+# if (k == l) or (l == m) or (k == m):
+#   print("yes")
+# else:
+#   print("no")
+#Zad 4
+
+# a, b, c, d, = int(input()), int(input()), int(input()), int(input())
+# lista = [a, b, c, d]
+# n = None
+# for i in lista:
+#   if n == None or n > i:
+#     n = i
+#   print("Najmniejsza liczba to:",n)
